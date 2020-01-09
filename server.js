8e946ab82dd832b23b16ea52b99817d2ede40bb6
Matty Watts <watts.mathew@gmail.com>
2020-01-08 18:13:06 -0600

    testing the app

---
+++ server.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var notes = []
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.get("/api/notes", function (req, res) {
+
+    fs.readFile("db/db.json", function (error, data) {
+        if (error) {
+            throw error;
+        };
+        let notes = JSON.parse(data);
+        return res.json(notes);
+    });
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+    fs.readFile("db/db.json", function (error, data) {
+        if (error) {
+            throw error;
+        };
+        let notes = JSON.parse(data);
+
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: shortid.generate()
+        }
+
+        notes.push(newNote);
+
+        fs.appendFile("db/db.json", JSON.stringify(notes, null, 2), (error) => {
+            if (error) {
+                throw error;
+            };
+            res.send('200');
+        });
+
+    });
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    let chosen = req.params.id;
+
+    fs.readFile("db/db.json", function (err, data) {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+
+        function searchChosen(chosen, notes) {
+            for (var i = 0; i < notes.length; i++) {
+                if (notes[i].id === chosen) {
+                    notes.splice(i, 1);
+                }
+            }
+        }
+
+        searchChosen(chosen, notes);
+
+        fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), (err) => {
+            if (err) throw err;
+            res.send('200');
+        });
+
+    });
+
+});
+
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
