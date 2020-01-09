0a6a28b1f199d95ca0bfe823f60fc8859f376e07
Matty Watts <watts.mathew@gmail.com>
2020-01-08 20:18:56 -0600

    rearanged the files and folders to be like the original folder we were given

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname,'public',)));
 
 
-app.get("*", function (_req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", function (_req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get("/notes", function (_req, res) {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 app.get("/api/notes", function (req, res) {
 
-    fs.readFile("db/db.json", function (error, data) {
+    fs.readFile("public/db/db.json", function (error, data) {
         if (error) {
             throw error;
         };
@@ -32,7 +32,7 @@ app.get("/api/notes", function (req, res) {
 
 app.post('/api/notes', (req, res) => {
 
-    fs.readFile("db/db.json", function (error, data) {
+    fs.readFile("public/db/db.json", function (error, data) {
         if (error) {
             throw error;
         };
@@ -46,7 +46,7 @@ app.post('/api/notes', (req, res) => {
 
         notes.push(newNote);
 
-        fs.appendFile("db/db.json", JSON.stringify(notes, null, 2), (error) => {
+        fs.appendFile("public/db/db.json", JSON.stringify(notes, null, 2), (error) => {
             if (error) {
                 throw error;
             };
@@ -56,34 +56,6 @@ app.post('/api/notes', (req, res) => {
     });
 
 });
-
-app.delete('/api/notes/:id', (req, res) => {
-    let chosen = req.params.id;
-
-    fs.readFile("db/db.json", function (err, data) {
-        if (err) throw err;
-        let notes = JSON.parse(data);
-
-        function searchChosen(chosen, notes) {
-            for (var i = 0; i < notes.length; i++) {
-                if (notes[i].id === chosen) {
-                    notes.splice(i, 1);
-                }
-            }
-        }
-
-        searchChosen(chosen, notes);
-
-        fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), (err) => {
-            if (err) throw err;
-            res.send('200');
-        });
-
-    });
-
-});
-
-
 
 
 app.listen(PORT, function () {
